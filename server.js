2710a2526b56f80bc9f342685ca69db5d2b4ea85
davidtrangit <davidtranwork123@gmail.com>
2025-08-09 17:19:08 +1000

    Update server.js

--- server.js
+++ server.js
@@ -44,3 +44,7 @@ app.post('/users/login', async (req, res) => {
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
